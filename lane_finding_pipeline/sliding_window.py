ebf297520eef2fa28b7e19191c482634a0207017
Gary Wang <kaiyuewang123@gmail.com>
2019-07-05 02:20:27 +0000

    changes

--- lane_finding_pipeline/sliding_window.py
+++ lane_finding_pipeline/sliding_window.py
@@ -28,47 +28,62 @@ class LaneFinding(PipeLineInterface):
         nonzero = image.nonzero()
         nonzeroy = np.array(nonzero[0])
         nonzerox = np.array(nonzero[1])
+        # Current positions to be updated later for each window in nwindows
+        leftx_current = leftx_base
+        rightx_current = rightx_base
 
-        leftx_current, rightx_current = leftx_base, rightx_base
+        # Create empty lists to receive left and right lane pixel indices
         left_lane_inds = []
         right_lane_inds = []
 
+        # Step through the windows one by one
         for window in range(self.nwindows):
-            win_y_low = image.shape[0] - (window + 1) * window_height
-            win_y_high = image.shape[0] - window * window_height
+            # Identify window boundaries in x and y (and right and left)
+            win_y_low = image.shape[0] - (window+1)*window_height
+            win_y_high = image.shape[0] - window*window_height
             win_xleft_low = leftx_current - self.nmargin
             win_xleft_high = leftx_current + self.nmargin
             win_xright_low = rightx_current - self.nmargin
             win_xright_high = rightx_current + self.nmargin
 
             # Draw the windows on the visualization image
-            cv2.rectangle(out_img, (win_xleft_low, win_y_low),
-                          (win_xleft_high, win_y_high), (0, 255, 0), 2)
-            cv2.rectangle(out_img, (win_xright_low, win_y_low),
-                          (win_xright_high, win_y_high), (0, 255, 0), 2)
+            cv2.rectangle(out_img,(win_xleft_low,win_y_low),
+            (win_xleft_high,win_y_high),(0,255,0), 2) 
+            cv2.rectangle(out_img,(win_xright_low,win_y_low),
+            (win_xright_high,win_y_high),(0,255,0), 2) 
 
             # Identify the nonzero pixels in x and y within the window #
-            good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
-                              (nonzerox >= win_xleft_low) & (nonzerox < win_xleft_high)).nonzero()[0]
-            good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
-                               (nonzerox >= win_xright_low) & (nonzerox < win_xright_high)).nonzero()[0]
+            good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & 
+            (nonzerox >= win_xleft_low) &  (nonzerox < win_xleft_high)).nonzero()[0]
+            good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & 
+            (nonzerox >= win_xright_low) &  (nonzerox < win_xright_high)).nonzero()[0]
 
+            # Append these indices to the lists
             left_lane_inds.append(good_left_inds)
             right_lane_inds.append(good_right_inds)
 
-            # If you found > minpix pixels, recenter next window on their mean position
+           # If you found > minpix pixels, recenter next window on their mean position
             if len(good_left_inds) > self.minpix:
                 leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
-            if len(good_right_inds) > self.minpix:
-                rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
+            if len(good_right_inds) > self.minpix:        
+                rightx_current = np.int(np.mean(nonzerox[good_right_inds])) 
+
+        # Concatenate the arrays of indices (previously was a list of lists of pixels)
+        try:
+            left_lane_inds = np.concatenate(left_lane_inds)
+            right_lane_inds = np.concatenate(right_lane_inds)
+        except ValueError:
+            # Avoids an error if the above is not implemented fully
+            pass
 
         # Extract left and right line pixel positions
         leftx = nonzerox[left_lane_inds]
-        lefty = nonzeroy[left_lane_inds]
+        lefty = nonzeroy[left_lane_inds] 
         rightx = nonzerox[right_lane_inds]
         righty = nonzeroy[right_lane_inds]
 
         return leftx, lefty, rightx, righty, out_img
+
 
     def process(self, image):
         '''
